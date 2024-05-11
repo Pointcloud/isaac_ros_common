3e8625edbc143d81b4a409225d0f19da8aa16246
Rob Roberts <967585+rob-pointcloud@users.noreply.github.com>
2024-05-11 10:45:28 +0900

    Update install-zed-aarch64.sh
    Signed-off-by: Rob Roberts <967585+rob-pointcloud@users.noreply.github.com>

--- docker/scripts/install-zed-aarch64.sh
+++ docker/scripts/install-zed-aarch64.sh
@@ -15,7 +15,7 @@ export USER=admin
 # sudo mkdir -p /etc/udev/rules.d/
 # sudo apt install udev
 
-chmod +x ZED_SDK.run ; /ssd/ros2_ws/ZED_SDK.run skip_od_module skip_python skip_drivers
+chmod +x /ssd/ros2_ws/ZED_SDK.run ; /ssd/ros2_ws/ZED_SDK.run skip_od_module skip_python skip_drivers
 
 # Symlink required to use the streaming features on Jetson inside a container, based on
 # https://github.com/stereolabs/zed-docker/blob/fd514606174d8bb09f21a229f1099205b284ecb6/4.X/l4t/devel/Dockerfile#L27C5-L27C95
